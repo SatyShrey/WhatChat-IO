b7d8ba745facb98cce2d6bcce87e09da1e2af311
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2024-10-04 22:37:41 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,118 +1,42 @@
-/* eslint-disable no-undef */
 
-require("dotenv").config()
-const express = require("express")
-const app = express()
-const cors = require('cors')
-app.use(cors())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-const mongoUrl = `mongodb+srv://sndsatya:QtAy7QbfwCnzUhvu@clustersnd.adfao0n.mongodb.net/`
-const Port = 3001
-const mongoClient = require('mongodb').MongoClient
+import { Server } from "socket.io";
+import { createServer } from "http";
 
-//get users................................................
-app.get('/users', (req, res) => {
-    mongoClient.connect(mongoUrl).then(clientObject => {
-        var database = clientObject.db('chatapp');
-        database.collection('users').find({}).toArray().then(data => {
-            res.send(data); res.end()
+const httpServer=createServer();
+const io=new Server(httpServer,{ cors:{origin:"*"}});
+
+let onlineusers=[];
+
+io.on("connection",(socket)=>{
+
+    socket.on('Emit',(data)=>{
+
+        if(onlineusers.includes(data)===false){
+            onlineusers.push(data)
+        }
+
+        
+        socket.on("disconnect",()=>{
+            onlineusers=onlineusers.filter(a=>a!==data)
+            io.emit("Emit",onlineusers);
         })
+
+        io.emit("Emit",onlineusers);
     })
-})
 
-//get chats................................................
-app.get('/chats/:id/:id2', (req, res) => {
-    var p1 = req.params.id;
-    var p2 = req.params.id2
-    mongoClient.connect(mongoUrl).then(clientObject => {
-        var database = clientObject.db('chatapp');
-        database.collection('chats').find({ $or: [{ p1: p1, p2: p2 }, { p1: p2, p2: p1 }] }).toArray().then(data => {
-            res.send(data); res.end()
-        })
+    socket.on('logout',(data)=>{
+        onlineusers=onlineusers.filter(a=>a!==data)
+        io.emit('logout',onlineusers)
     })
-})
 
-//get group chats
-app.get('/groupchats/:id', (req, res) => {
-    mongoClient.connect(mongoUrl).then(clientObject => {
-        var database = clientObject.db('chatapp');
-        database.collection('groupchats').find({groupid:req.params.id}).toArray().then(data => {
-            res.send(data); res.end()
-        })
+    socket.on("chat",(data)=>{
+        io.emit("chat",data)
     })
-})
 
-///add group chats
-app.post('/addgroupchat', (req, res) => {
-    mongoClient.connect(mongoUrl).then(clientObject => {
-        var database = clientObject.db('chatapp');
-        database.collection('groupchats').insertOne(req.body);
-        res.end();
+    socket.on("groupchat",(data)=>{
+        io.emit("groupchat",data)
     })
+
 })
 
-//post chats...........................
-app.post('/addchat', (req, res) => {
-    mongoClient.connect(mongoUrl).then(clientObject => {
-        var database = clientObject.db('chatapp');
-        database.collection('chats').insertOne(req.body);
-        res.end();
-    })
-})
-
-//signup
-app.post('/signup', (req, res) => {
-    mongoClient.connect(mongoUrl).then(clientObject => {
-        var database = clientObject.db('chatapp');
-        database.collection('users').insertOne(req.body);
-    }).then((data)=>{res.send(data);res.end();})
-})
-
-//update User data////////////////////////////////////////////////////
-app.patch('/updateuser/:id', (req, res) => {
-    mongoClient.connect(mongoUrl).then(clientObject => {
-        var database = clientObject.db('chatapp');
-        database.collection('users').updateOne({id:req.params.id},{$set:req.body});
-        res.end();
-    })
-})
-////////////////////////////////////////////////////////////////////////
-//login
-app.get('/login/:id/:pw',(req,res)=>{
-    mongoClient.connect(mongoUrl).then(clientObject => {
-        var database = clientObject.db('chatapp');
-        database.collection('users').find({email:req.params.id,pw:req.params.pw}).toArray().then(data => {
-        res.send(data);res.end()
-        })
-    })
-})
-
-
-//Check user
-app.get('/exist/:id/', (req, res) => {
-    mongoClient.connect(mongoUrl).then(clientObject => {
-        var database = clientObject.db('chatapp');
-        database.collection('users').find({email:req.params.id}).toArray().then(data => {
-          if(data.length>0){
-            res.send("true");res.end
-          }else{res.send("false");res.end;}
-        })
-    })
-})
-
-//check groups
-app.get('/group/:id/', (req, res) => {
-    mongoClient.connect(mongoUrl).then(clientObject => {
-        var database = clientObject.db('chatapp');
-        database.collection('users').find({group:req.params.id}).toArray().then(data => {
-          if(data.length>0){
-            res.send("true");res.end
-          }else{res.send("false");res.end;}
-        })
-    })
-})
-
-
-
-app.listen(Port, () => { console.log(`Mongo server started:${Port}`) })
+httpServer.listen(4001,()=>{console.log(`server started:4001`)})
